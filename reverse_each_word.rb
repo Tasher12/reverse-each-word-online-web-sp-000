3b1c548f48d02b59b28dcdc93f2f7ff5cc8f59f4
authentic-pipe-8414 <tasher.stidham@outlook.com>
2021-01-19 17:22:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,5 @@
 def reverse_each_word(string)
+<<<<<<< HEAD
   string = string.split(" ")
   reversed = []
   string.each {|i| reversed << i.reverse} 
@@ -10,4 +11,10 @@ def reverse_each_word(string)
   reversed = []
   string.collect {|i| reversed << i.reverse} 
   return reversed.join(" ")
+=======
+  my_statements = "Hello there, and how are you?".split 
+  my_statements.each do |str|
+    my_statements << str.reverse
+  end
+>>>>>>> 8eed599b82882a9b2a1a253f710d22b83824d182
 end
